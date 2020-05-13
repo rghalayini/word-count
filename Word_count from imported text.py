fce39fe2d49b86fbf8b9d665b67594b83186eefe
Rémy Ghalayini <63195370+rghalayini@users.noreply.github.com>
2020-05-13 16:30:17 +0200

    Add files via upload
    This a variation of the manual file, whereby this code will count the words of .txt file

---
+++ Word_count from imported text.py
@@ -0,0 +1,22 @@
+def word_count(filename):
+    from collections import Counter
+    with open (filename, 'r') as f:
+        text = f.read()
+        skips = [".", ",", ";", ":", "'",'"', "!", "?", "(", ")"]
+        text.replace("\n", " ")
+        text=text.lower()
+        for ch in skips:
+            text = text.replace(ch,"")
+        word_counts=Counter(text.split())
+        x=0
+        for value in word_counts.values():
+            x=x+value
+        print("The number of words in this file is:", x)
+        for k, v in word_counts.items():
+            if v==max(word_counts.values()):
+                print("The most frequent word is", k, "and was repeated", v, "times") 
+            else:
+                None        
+    return word_counts
+
+#call the function by specifying the .txt file you want to read
